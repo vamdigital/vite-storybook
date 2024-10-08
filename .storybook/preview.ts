f28fcb537bbc9220a303d3a844ca1616a53c24ba
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-10-08 16:26:05 +0100

    feat(storybook): :sparkles: adds introduction page and also adds another color for theme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -8,6 +8,17 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    backgrounds: {
+      default: "Dark",
+      values: [
+        { name: "Dark", value: "#333" },
+        { name: "Light", value: "#F7F9F2" },
+        { name: "Maroon", value: "#F00000" },
+      ],
+    },
+  },
+  globals: {
+    backgrounds: { value: "maroon" },
   },
 };
 
